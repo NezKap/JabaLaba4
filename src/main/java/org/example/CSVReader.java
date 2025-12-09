52d00a57eb72c4708d31569a9b09869ceb48b31b
NezKap <werras206@mail.ru>
2025-12-09 22:05:05 +0300

    Добавлен конструктор в класс CSVReader, методы получения и установления имени файла, а также метод чтения данных из представленного CSV файла в классе CSVReader.

--- src/main/java/org/example/CSVReader.java
+++ src/main/java/org/example/CSVReader.java
@@ -1,7 +1,35 @@
 package org.example;
 
 import java.util.*;
+import java.io.*;
 
 public class CSVReader {
-
+    private List<Person> result;
+    private String fileName;
+    public CSVReader(String _fileName) {
+        fileName = _fileName;
+        result = new ArrayList<>();
+    }
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String _fileName) {
+        fileName = _fileName;
+    }
+    public List<Person> input() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
+        String str = scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            str = scanner.nextLine();
+            str = str.replace(" ", "");
+            if (!str.isEmpty()) {
+                String[] cols = str.split(";");
+                Person person = new Person(Integer.parseInt(cols[0]), cols[1], cols[2], new Department(cols[4]),
+                        Integer.parseInt(cols[5]), cols[4]);
+                result.add(person);
+            }
+        }
+        scanner.close();
+        return result;
+    }
 }
